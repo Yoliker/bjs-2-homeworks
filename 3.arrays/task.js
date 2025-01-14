bdc2f91ae15b665bab551b15d4c7255dbbaaefd1
Yuliya Bel <belodub.yuliya@gmail.com>
2025-01-14 14:48:10 +0300

    Task4-Arrow done

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,17 @@
 function compareArrays(arr1, arr2) {
-  
+	if (arr1.length === arr2.length) {
+		return arr1.every((element, index) => element === arr2[index]);
+	} else {
+		return false;
+	}
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    const usersGender = users.filter(user => user.gender === gender);
+	if (usersGender.length === 0) {
+		return 0;
+	}
+	const usersAges = usersGender.map(user => user.age);
+	const averageAge = (usersAges.reduce((acc, age) => (acc + age), 0)) / usersGender.length;
+	return averageAge;
 }
